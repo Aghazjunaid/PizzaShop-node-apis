b210fe84166563a878afb4250fa1746111a09c10
Aghaz1-alphonic <aghaz.junaid@alphonic.in>
2021-08-11 19:20:00 +0530

    product

---
+++ models/product.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+var ProductSchema = new Schema({
+    name: {type:String, required:true},
+    price: {type: Number, required: true},
+    image: {type: String},
+    description: {type: String},
+}, {timestamps: true});
+
+module.exports = mongoose.model("product", ProductSchema)
