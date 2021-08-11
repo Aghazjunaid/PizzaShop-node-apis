d588923f0f3c81d79eafb55e8be6614229d2e12e
Aghaz1-alphonic <aghaz.junaid@alphonic.in>
2021-08-11 19:29:17 +0530

    add product api

--- models/product.js
+++ models/product.js
@@ -8,4 +8,4 @@ var ProductSchema = new Schema({
     description: {type: String},
 }, {timestamps: true});
 
-module.exports = mongoose.model("product", ProductSchema)
+module.exports = mongoose.model("Product", ProductSchema)
