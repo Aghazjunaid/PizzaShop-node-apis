097b7dac621cd1bf78487d4491cf1ff4ab0c883f
Aghaz1-alphonic <aghaz.junaid@alphonic.in>
2021-08-12 22:24:39 +0530

    cart get

--- routes/cart.js
+++ routes/cart.js
@@ -32,11 +32,38 @@ module.exports = () => {
     }
 }
 
+ //====================get cart==========================================================
+ async function getCart(req,res){
+    var return_response = {
+        "status": null,
+        "message": null,
+        "data": null
+    }
+    try{
+        Cart.find({}).populate("product").exec(function(error,doc){
+            if(error){
+                return_response["status"] = 400;
+                return_response["message"] = String(error);
+                return res.json(return_response);
+            } else {
+                return_response["status"] = 200;
+                return_response["message"] = "success";
+                return_response["data"] = doc;
+                return res.json(return_response);
+            }
+        })
+    } catch (error) {
+        return_response["message"] = String(error);
+        return res.status(400).json(return_response);
+    }
+}
+
 
 
 
     return {
         postCart,
+        getCart
     }
 
 }
